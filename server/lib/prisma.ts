c42effcb5569cce32059a1d403545c6d5d95a8ac
Aphichet07 <aphichet254888@gmail.com>
2026-06-05 04:24:53 +0700

    chore: fix db

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -3,7 +3,6 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client/client.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
-console.log("test"+process.env.DATABASE_URL)
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
